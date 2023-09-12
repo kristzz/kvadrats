55248e81394903d430600894cdb48d2eeceae346
kristzz <emils.krists@gmail.com>
2023-09-12 22:37:57 +0300

    bez animacijas

---
+++ script.js
@@ -0,0 +1,26 @@
+let field = document.getElementById('field');
+let kubiks = document.getElementById('kubiks');
+let btn = document.getElementById('poga');
+
+let counter = 0;
+
+btn.addEventListener('click', function(){
+    counter++;
+    btn.textContent = "Spied te: " + counter;
+
+    let maxX = field.clientWidth - kubiks.offsetWidth - 100;
+    let maxY = field.clientHeight - kubiks.offsetHeight - 100;
+
+    let randomX = Math.random() * maxX;
+    let randomY = Math.random() * maxY;
+
+    let randomRotation = Math.random() * 360;
+
+    let percentX = (randomX / field.clientWidth) * 100 + 1;
+    let percentY = (randomY / field.clientHeight) * 100 + 1;
+
+    kubiks.style.left = percentX + "%";
+    kubiks.style.top = percentY + "%";
+    kubiks.style.transform = `rotate(${randomRotation}deg)`;
+
+})
